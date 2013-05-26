d1bea5a8e70116e3db8d995f8cd3e3d181159fac
Dylan Cochran <heliocentric@gmail.com>
2013-05-26 15:40:05 -0400

    Add api.php

--- win-monitor/ClassLibrary1/WinMonitor.cs
+++ win-monitor/ClassLibrary1/WinMonitor.cs
@@ -9,12 +9,16 @@ namespace winmonitorlib
 {
     public class WinMonitor
     {
+        private WRegistry registry = new WRegistry();
         private WebClient client;
         public void Start()
         {
+            String URI = registry.URL + "/api.php";
             this.client = new WebClient();
-            
-
+            System.Collections.Specialized.NameValueCollection reqparm = new System.Collections.Specialized.NameValueCollection();
+            reqparm.Add();
+            byte[] responsebytes = client.UploadValues(URI, "POST", reqparm);
+            string responsebody = Encoding.UTF8.GetString(responsebytes);
         }
         public void Stop()
         {
