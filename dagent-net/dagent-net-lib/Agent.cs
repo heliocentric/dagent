5fab49f2d1af6f09405872738e743d82d080db97
Dylan Cochran <heliocentric@gmail.com>
2013-08-22 18:33:12 -0400

    Add agent class

---
+++ dagent-net/dagent-net-lib/Agent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace dagent_net_lib
+{
+    public class Agent
+    {
+        public void Run()
+        {
+
+        }
+
+        public void Start()
+        {
+
+        }
+
+        public void Stop()
+        {
+
+        }
+    }
+}
