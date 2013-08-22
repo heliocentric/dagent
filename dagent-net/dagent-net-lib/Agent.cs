3a06773b64437de550fc24c59d6d281f6b7c60d3
Dylan Cochran <heliocentric@gmail.com>
2013-08-22 18:53:03 -0400

    Add test shim application

--- dagent-net/dagent-net-lib/Agent.cs
+++ dagent-net/dagent-net-lib/Agent.cs
@@ -2,24 +2,50 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 
 namespace dagent_net_lib
 {
     public class Agent
     {
+        private Thread Main;
         public void Run()
         {
 
         }
+        /* This function contains the start routines for the actual agent thread. 
+         * Note, that only init does this, and you only want to init once, normally
+         * when the computer is started. 
+         * 
+         * The agent will then begin it's initialization routine, which involves
+         * sitting on a named pipe waiting for further instructions from this class.
+         * This allows the code to be in both an embedded library, and a service. 
+         * 
+         */
 
+        private void realStart()
+        {
+        }
+        public void Init(BOOL)
+        {
+            this.Main = new Thread(new ThreadStart(this.realStart));
+            this.Main.Start();
+        }
         public void Start()
         {
-
         }
 
         public void Stop()
         {
-
+        }
+        public void Pause()
+        {
+        }
+        public void Resume()
+        {
+        }
+        public void ForceSync()
+        {
         }
     }
 }
---
+++ dagent-net/dagent-test-shim/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using dagent_net_lib;
+namespace dagent_test_shim
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Main = new Agent();
+            this.Main.Init();
+        }
+        private Agent Main;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Main.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Main.Stop();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Main.Pause();
+        }
+
+    }
+}
